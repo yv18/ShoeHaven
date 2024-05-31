b98d732d4c0a469ad376016a9f25472cbf4e4294
Yashraj Raj <rajrishi2329@gmail.com>
2024-05-31 00:40:50 -0400

    Update the setup for Frontend and Backend

--- Frontend/SRC/Route.jsx
+++ Frontend/SRC/Route.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
-import EmployeeDirectory from "./Components/EmployeeDirectory.jsx";
-import EmployeeCreate from "./Components/EmployeeCreate.jsx";
-import EmployeeUpdate from "./Components/EmployeeUpdate.jsx";
-import EmployeeTable from "./Components/EmployeeTable.jsx";
 import Home from "./Components/Home.jsx";
 
 
@@ -14,10 +10,6 @@ function LinksSetup() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/AddNewEmployee" element={<EmployeeCreate />} />
-        <Route path="/filter" element={<EmployeeDirectory />} />
-        <Route path="/update" element={<EmployeeUpdate />} />
-          <Route path=':EmpID' element={<EmployeeUpdate />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
