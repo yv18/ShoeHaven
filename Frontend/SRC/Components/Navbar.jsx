4b905822ea4e41e945d08b50c34537137d20326c
Aishwarya3005 <aishmayavanshi3005@gmail.com>
2024-06-11 13:20:57 -0400

    Login Signup

--- Frontend/SRC/Components/Navbar.jsx
+++ Frontend/SRC/Components/Navbar.jsx
@@ -1,43 +1,21 @@
-import React, { useRef } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
-import logo from "../Assets/logo.png";
-import nav_dropdown from "../Assets/drop_nav.jpeg";
-import cart_icon from "../Assets/cart.jpeg";
 
 function NavigationBar() {
-  const menuRef = useRef();
-  const dropdownBtn = (e) => {
-    menuRef.current.classList.toggle('nav-menu-visible');
-    e.target.classList.toggle('open');
-  };
-
-  const handleLogout = () => {
-    localStorage.removeItem('auth-token');
-    window.location.replace("/");
-  };
-
   return (
-    <div className="nav">
-      <Link to="/" style={{ textDecoration: "none" }} className="nav-logo">
-        <img src={logo} alt="logo" />
-        <p>ShoeHaven</p>
-      </Link>
-      <img onClick={dropdownBtn} className="nav-dropdown" src={nav_dropdown} alt="dropdown" />
-      <ul ref={menuRef} className="nav-menu">
-        <li><Link to="/" style={{ textDecoration: "none", color:"black" }}>All</Link></li>
-        <li><Link to="/Men" style={{ textDecoration: "none", color:"black" }}>Men</Link></li>
-        <li><Link to="/Female" style={{ textDecoration: "none", color:"black" }}>Women</Link></li>
-      </ul>
-      <div className="nav-login-cart">
-        {localStorage.getItem('auth-token')
-          ? <button onClick={handleLogout}>Logout</button>
-          : <Link to="/login" style={{ textDecoration: "none" }}><button>Login</button></Link>}
-        <Link to="/cart"><img src={cart_icon} alt="cart" /></Link>
-        <div className="nav-cart-count">0</div>
-      </div>
-    </div>
+    <>
+      <nav>
+        <div>
+          <ul className="YV" style={{ listStyleType: "none"}}>
+            <li><Link to="/" style={{color:"white"}}>Home</Link></li>
+            <li><Link to="/AddNewEmployee"  style={{color:"white"}}>Add New Employee</Link></li>
+            <li><Link to="/filter"  style={{color:"white"}}>Filter & Update</Link></li>
+          </ul>   
+        </div>
+      </nav>
+    </>
   );
 }
 
-
+// this one is for Navigation bar for user to redirect them from one page to another
 export default NavigationBar;
