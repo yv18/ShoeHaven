1d0041ecf36b681043d33186fa25b2b2fbde15da
Yashraj Raj <147085086+yv18@users.noreply.github.com>
2024-08-12 19:05:50 -0400

    Update webpack.config.js

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -1,15 +1,20 @@
 const path = require('path');
 const webpack = require('webpack');
+const TerserPlugin = require('terser-webpack-plugin');
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === 'production';
 
   return {
     mode: isProduction ? 'production' : 'development',
-    entry: { app: './SRC/App.jsx' },
+    entry: {
+      app: './SRC/App.jsx',
+    },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'public'),
+      publicPath: '/',
     },
     module: {
       rules: [
@@ -56,13 +61,35 @@ module.exports = (env, argv) => {
       ],
     },
     optimization: {
-      splitChunks: { name: 'vendor', chunks: 'all' },
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendor',
+            chunks: 'all',
+            enforce: true,
+          },
+          common: {
+            name: 'common',
+            minChunks: 2,
+            chunks: 'all',
+            enforce: true,
+          },
+        },
+      },
+      minimize: isProduction,
+      minimizer: [new TerserPlugin()],
     },
     plugins: [
       new webpack.DefinePlugin({
         __isBrowser__: 'true',
       }),
+      // Only include the BundleAnalyzerPlugin in production mode
+      ...(isProduction ? [new BundleAnalyzerPlugin()] : []),
     ],
     devtool: isProduction ? 'source-map' : 'eval-source-map',
+    resolve: {
+      extensions: ['.js', '.jsx'],
+    },
   };
 };
