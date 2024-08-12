a991c1170b40e05eba39b1bc897e582a2c913228
Yashraj Raj <147085086+yv18@users.noreply.github.com>
2024-08-12 17:50:12 -0400

    Update signup.jsx

--- Frontend/SRC/Components/signup.jsx
+++ Frontend/SRC/Components/signup.jsx
@@ -17,7 +17,7 @@ const LoginSignup = () => {
 
   const login = async () => {
     try {
-      const response = await fetch('http://localhost:8000/login', {
+      const response = await fetch('https://shoehaven-backend.onrender.com/login', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -44,7 +44,7 @@ const LoginSignup = () => {
 
   const signup = async () => {
     try {
-      const response = await fetch('http://localhost:8000/signup', {
+      const response = await fetch('https://shoehaven-backend.onrender.com/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
