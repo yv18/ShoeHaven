995ca7ef80a198feafa2bcb56c516f41fec4f5d8
Yashraj Raj <147085086+yv18@users.noreply.github.com>
2024-08-12 19:00:13 -0400

    Update webpack.config.js

--- Frontend/webpack.config.js
+++ Frontend/webpack.config.js
@@ -1,61 +1,68 @@
 const path = require('path');
 const webpack = require('webpack');
 
-module.exports = {
-  mode: 'development',
-  entry: { app: './SRC/App.jsx' },
-  output: {
-    filename: '[name].bundle.js',
-    path: path.resolve(__dirname, 'public'),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(jpe?g|png|gif|svg|ico)$/i,
-        use: [
-          {
-            loader: 'file-loader',
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
+
+  return {
+    mode: isProduction ? 'production' : 'development',
+    entry: { app: './SRC/App.jsx' },
+    output: {
+      filename: '[name].bundle.js',
+      path: path.resolve(__dirname, 'public'),
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(jpe?g|png|gif|svg|ico)$/i,
+          use: [
+            {
+              loader: 'file-loader',
+              options: {
+                name: '[name].[ext]',
+                outputPath: 'img/',
+              },
+            },
+          ],
+        },
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
             options: {
-              name: '[name].[ext]',
-              outputPath: 'img/',
+              presets: [
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: {
+                      ie: '11',
+                      edge: '15',
+                      safari: '10',
+                      firefox: '50',
+                      chrome: '49',
+                    },
+                  },
+                ],
+                '@babel/preset-react',
+              ],
             },
           },
-        ],
-      },
-      {
-        test: /\.jsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: [
-              ['@babel/preset-env', {
-                targets: {
-                  ie: '11',
-                  edge: '15',
-                  safari: '10',
-                  firefox: '50',
-                  chrome: '49',
-                },
-              }],
-              '@babel/preset-react',
-            ],
-          },
         },
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+    optimization: {
+      splitChunks: { name: 'vendor', chunks: 'all' },
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        __isBrowser__: 'true',
+      }),
     ],
-  },
-  optimization: {
-    splitChunks: { name: 'vendor', chunks: 'all' },
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      __isBrowser__: 'true',
-    }),
-  ],
-  devtool: 'source-map',
+    devtool: isProduction ? 'source-map' : 'eval-source-map',
+  };
 };
